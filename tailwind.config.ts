e84dd8b0acb221fcf46f95e285418dc8b9d299b2
harshith107 <harshuharshithmic0803@outlook.com>
2025-04-06 00:50:29 +0530

    This is 2nd push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
-// tailwind.config.ts
 export default {
-    darkMode: 'class',
     content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  }  
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
